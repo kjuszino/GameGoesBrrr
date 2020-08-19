518646387ad69e5433066227a6980d40f408f5de
toddb <toddbrown2002@gmail.com>
2020-08-19 16:15:22 +0100

    added doge changed time between attacks included doge to fight

--- GameGoesBrrr/src/Dog.java
+++ GameGoesBrrr/src/Dog.java
@@ -1,3 +1,43 @@
-public class Dog {
+public class Dog implements Enemy, Damageable {
+    private int health;
+    private int damage;
 
+    public Dog(){
+        this.health = 16;
+        this.damage = 9;
+    }
+    @Override
+    public void takeDamage(int damage) {
+        if(this.health-damage>=0){
+            this.health -=damage;
+        }else{
+            this.health = 0;
+        }
+
+    }
+
+    @Override
+    public int getLoot() {
+        return 1;
+    }
+
+    @Override
+    public int getHealth() {
+        return this.health;
+    }
+
+    @Override
+    public int getDamage() {
+        return this.damage;
+    }
+
+    @Override
+    public void attack(Damageable damageable){
+        damageable.takeDamage(this.damage);
+    }
+
+    @Override
+    public String toString(){
+        return "Dog";
+    }
 }
--- GameGoesBrrr/src/UserInterface.java
+++ GameGoesBrrr/src/UserInterface.java
@@ -23,7 +23,7 @@ public class UserInterface {
                 eventDrawer();
                 advance = scanner.nextLine();
             }
-
+            break;
 
         }
 
@@ -35,8 +35,10 @@ public class UserInterface {
         int encounter = random.nextInt(1000);
         if(encounter<=499){
             System.out.println("Nothing happens");
-        }else {
+        }else if(encounter>501 && encounter<=502){
             fight(new Goblin());
+        }else if(encounter>=503 && encounter<=1000){
+            fight(new Dog());
         }
     }
 
@@ -44,18 +46,18 @@ public class UserInterface {
         while(enemy.getHealth()>0 && this.mainCharacter.getHealth()>0) {
             this.mainCharacter.attack((Damageable) enemy);
             System.out.println(this.mainCharacter.getName() + " attacked " + enemy + " and dealt " + this.mainCharacter.getDamage() + " damage");
-            Thread.sleep(300);
+            Thread.sleep(400);
             System.out.println(enemy + " has " + enemy.getHealth() + " health left");
-            Thread.sleep(500);
+            Thread.sleep(1000);
             if (enemy.getHealth() < 1) {
                 break;
             }
             System.out.println();
             enemy.attack(this.mainCharacter);
             System.out.println(enemy + " attacked " + this.mainCharacter.getName() + " and dealt " + enemy.getDamage() + " damage");
-            Thread.sleep(300);
+            Thread.sleep(400);
             System.out.println(this.mainCharacter.getName() + " has " + this.mainCharacter.getHealth() + " health left");
-            Thread.sleep(500);
+            Thread.sleep(1000);
             System.out.println();
 
         }
